34b263c3a4e27e3dd3e6c2d2b83d2b09a8734871
xosemanolo <xose.manolo.rodriguez@gmail.com>
2021-12-16 17:29:11 -0500

    check DB connection, attempting to resolve issue

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
+    console.log('connected to database');
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
